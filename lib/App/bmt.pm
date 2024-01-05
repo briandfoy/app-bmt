a23977f60ba8a591242a632f6269ed4c8085000d
brian d foy <briandfoy@pobox.com>
2024-01-05 09:46:34 -0500

    Add stub Pod to module

--- lib/App/bmt.pm
+++ lib/App/bmt.pm
@@ -2,6 +2,16 @@ package App::bmt;
 
 our $VERSION = '1.003';
 
+=head1 NAME
+
+App::bmt - brian's module too
+
+=head1 SYNOPSIS
+
+	$ bmt
+
+=cut
+
 # This module only exists to create a namespace
 
 1;
